5c30cc3830d9abb942f9e33a561e0e9b4351f9c6
Rene Reyes Cañeda <canedarene@gmail.com>
2025-02-04 06:27:14 -0600

    creacion del codigo

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-alert ('Hola mundo');
+//varaiables
+let numeroSecreto = 6;
+let numeroUser = prompt("Me indicas un numero entre el 1 y e 10: ");
+
+//alert ('Hola mundo');
+//Cuerpo de programación
+console.log(numeroUser);
+if(numeroUser == numeroSecreto){
+    alert(`Acertaste, el número es: $(numeroUser)`);
+} else{
+    alert("No acertaste del juego")
+}
