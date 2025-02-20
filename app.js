a058f5bb62bb8e8c5a42e13291cd22873ec8f840
Rene Reyes Cañeda <canedarene@gmail.com>
2025-02-20 13:23:34 -0600

    nueva funcion intentoDeUsuario

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ titulo.innerHTML = '¿Podrás adivinar el número sercreto?';
 
 let parrafo = document.querySelector('h2');
 titulo.innerHTML = 'Escoge un numero entre el 1 y el 10';
+
+function intentoDeUsuario(){
+    alert('Click desde la funcion');
+}
